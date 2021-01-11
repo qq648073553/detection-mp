b39639fdbfd471e11f0d13d914c4d4097fd17dc0
zfd <zfd_yes@163.com>
2021-01-11 14:19:44 +0800

    version 1.0 deprecated

--- pages/function-list/function-list.js
+++ pages/function-list/function-list.js
@@ -1,18 +1,106 @@
 // pages/function-list/function-list.js
+
+const detectionList = [
+      {
+        title: '工程信息登记',
+        icon: 'dengji',
+        color: '#F0641F'
+      },
+      {
+        title: '工程信息查看',
+        icon: 'chakan',
+        color: '#3FABFF'
+      },
+      {
+        title: '委托信息登记',
+        icon: 'weituo',
+        color: '#F0641F'
+      },
+      {
+        title: '委托信息查看',
+        icon: 'chakan',
+        color: '#3FABFF'
+      },
+      {
+        title: '委托信息修改',
+        icon: 'xiugai',
+        color: '#DA251D'
+      },
+      {
+        title: '检测进度查询',
+        icon: 'jindu',
+        color: '#3DC393'
+      },
+      {
+        title: '现场检测预约',
+        icon: 'yuyue',
+        color: '#6445E3'
+      }
+    ]
+const reportList = [
+      {
+        title: '报告检索',
+        icon: 'jiansuo',
+        color: '#3FABFF'
+      },
+      {
+        title: '防伪认证',
+        icon: 'renzheng',
+        color: '#F0641F'
+      },
+      {
+        title: '结果与结论查询',
+        icon: 'jieguo',
+        color: '#3DC393'
+      },
+      {
+        title: '报告下载',
+        icon: 'xiazai',
+        color: '#DA251D'
+      }
+    ]
+const balanceList = [
+      {
+        title: '工作量汇总',
+        icon: 'huizong',
+        color: '#3FABFF'
+      },
+      {
+        title: '明细查看',
+        icon: 'mingxi',
+        color: '#F0641F'
+      },
+      {
+        title: '结算清单',
+        icon: 'jiesuan',
+        color: '#3DC393'
+      }
+    ]
+let listMap = new Map([[0,detectionList],[1,reportList], [2, balanceList]])
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    activeKey: 0,
+    activeList: []
+  },
+  onChange(event) {
+    this.setData({
+      activeList:listMap.get(event.detail)
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      const key = +options.type || 0
+        this.setData({
+        activeKey:key,
+        activeList:listMap.get(key)
+        })
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,59 @@ Component({
     options: {
         styleIsolation: 'shared',
     },
+    data:{
+        business:[
+            {
+                title: '检测业务',
+                type: 0,
+                subList:[
+                    {
+                        title: '工程登记',
+                        icon: 'dengji',
+                        color: '#F0641F',
+                        url:'/pages/check-in/check-in'
+                    },
+                    {
+                        title: '工程查看',
+                        icon: 'chakan',
+                        color: '#3FABFF'
+                    },
+                ]
+            },
+            {
+                title: '报告业务',
+                type: 1,
+                subList:[
+                    {
+                        title: '报告检索',
+                        icon: 'jiansuo',
+                        color: '#3FABFF'
+                    },
+                    {
+                        title: '防伪认证',
+                        icon: 'renzheng',
+                        color: '#F0641F'
+                    },
+                ]
+            },
+            {
+                title: '结算业务',
+                type: 2,
+                subList:[
+                    {
+                        title: '工作量汇总',
+                        icon: 'huizong',
+                        color: '#3FABFF'
+                    },
+                    {
+                        title: '结算清单',
+                        icon: 'jiesuan',
+                        color: '#3DC393'
+                    },
+                ]
+            }
+        ]
+    },
     pageLifetimes: {
       show() {
         if (typeof this.getTabBar === 'function' &&
@@ -14,7 +67,9 @@ Component({
     },
     methods: {
         goPage(event) {
-            console.log(event)
+            wx.navigateTo({
+                url:event.currentTarget.dataset.url
+            })
         }
 
     }
